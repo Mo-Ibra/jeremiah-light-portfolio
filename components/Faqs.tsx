992e335543b55e582459fce39d4b95006ce54d8b
Mo-Ibra <mohamedelgedwy245@gmail.com>
2026-03-01 21:11:13 +0200

    Fix issues

--- components/Faqs.tsx
+++ components/Faqs.tsx
@@ -17,7 +17,7 @@ export default function FAQs() {
   return (
     <section
       id="faqs"
-      className="md:py-16 pt-8 md:pt-4 pb-8 bg-background relative overflow-hidden z-50"
+      className="md:py-12 py-8 bg-background relative overflow-hidden z-50"
     >
       <div className="absolute inset-0 flex items-center justify-center">
         <img
