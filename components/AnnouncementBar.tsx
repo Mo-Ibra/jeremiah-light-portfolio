0b951bca5080c39d20973b9bdaa59b6d10cc7077
Mo-Ibra <mohamedelgedwy245@gmail.com>
2026-03-08 15:06:33 +0200

    Reduce announcementbar height

--- components/AnnouncementBar.tsx
+++ components/AnnouncementBar.tsx
@@ -1,6 +1,6 @@
 const AnnouncementBar = () => {
   return (
-    < div className="w-full md:h-10 h-8 bg-primary flex items-center justify-center text-white md:text-sm text-xs font-medium z-50 relative" >
+    < div className="w-full md:h-8 h-7 bg-primary flex items-center justify-center text-white md:text-sm text-xs font-medium z-50 relative" >
       <span className="flex items-center gap-1">
         <span className="w-2 h-2 rounded-full bg-white animate-pulse" />
         Partnering with<span className="font-semibold text-white">1 more new brand</span>this month
